7a191fb2c97dd1ffb400d417376fba882edcb683
Niria <narutow21@interia.pl>
2021-01-23 14:35:08 +0100

    Added public dist

---
+++ build/precache-manifest.b7422905c918476b458171aa395f5069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518dab1e6843a13ffb03d6456de60d27",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b7efa0b1864bf7a1677",
+    "url": "./static/css/main.d319b5a4.chunk.css"
+  },
+  {
+    "revision": "45b623c42d1c0ab62c62",
+    "url": "./static/js/2.3e1047bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.3e1047bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7efa0b1864bf7a1677",
+    "url": "./static/js/main.5045ec6e.chunk.js"
+  },
+  {
+    "revision": "987a0ad2f865ef8508b3",
+    "url": "./static/js/runtime-main.c6c16f59.js"
+  }
+]);
